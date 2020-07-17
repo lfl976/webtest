ecaeb77ce289efd0d3712255c4c1f85261adf24c
v_fulinoliu <v_fulinoliu@tencent.com>
2020-07-17 22:11:14 +0800

    car

--- main.js
+++ main.js
@@ -1,59 +1,7 @@
-import { Carousel } from "./carousel.view";
-function create(Cls, attributes, ...children) {
-  let o;
-  if (typeof Cls === "string") {
-    o = new Wrapper(Cls);
-  } else {
-    o = new Cls({
-      timer: {},
-    });
-  }
+import { create, Wrapper, Text } from "./create";
+// import { Carousel } from "./carousel.view";
 
-  for (let name in attributes) {
-    o.setAttribute(name, attributes[name]);
-  }
-
-  for (let child of children) {
-    // o.children.push(child);
-    if (typeof child === "string") child = new Text(child);
-    o.appendChild(child);
-  }
-  return o;
-}
-
-class Text {
-  constructor(text) {
-    this.children = [];
-    this.root = document.createTextNode(text);
-  }
-  mountTo(parent) {
-    parent.appendChild(this.root);
-  }
-}
-
-class Wrapper {
-  constructor(type) {
-    this.children = [];
-    this.root = document.createElement(type);
-  }
-
-  setAttribute(name, value) {
-    this.root.setAttribute(name, value);
-  }
-
-  appendChild(child) {
-    this.children.push(child);
-  }
-
-  mountTo(parent) {
-    parent.appendChild(this.root);
-    for (let child of this.children) {
-      child.mountTo(this.root);
-    }
-  }
-}
-
-class MyComponent {
+/*class MyComponent {
   constructor(config) {
     this.children = [];
     // this.root = document.createElement("div");
@@ -85,6 +33,66 @@ class MyComponent {
     }
     this.render().mountTo(parent);
   }
+}*/
+
+class Carousel {
+  constructor(config) {
+    this.children = [];
+    this.attributes = new Map();
+    this.properties = new Map();
+  }
+
+  setAttribute(name, value) {
+    // this.root.setAttribute(name, value);
+    // this.attributes.set(name, value);
+    this[name] = value;
+  }
+
+  appendChild(child) {
+    // child.mountTo(this.root);
+    this.children.push(child);
+  }
+
+  render() {
+    let children = this.data.map((url) => {
+      let element = <img src={url} />;
+      element.addEventListener("dragstart", (event) => event.preventDefault());
+      return element;
+    });
+    let root = <div class="carousel">{children}</div>;
+
+    let position = 0;
+
+    let nextPic = () => {
+      let nextPosition = (position + 1) % this.data.length;
+      let current = children[position];
+      let next = children[nextPosition];
+
+      current.style.transition = "ease 0s";
+      next.style.transition = "ease 0s";
+
+      current.style.transform = `translateX(${-100 * position}%)`;
+      next.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
+
+      setTimeout(function () {
+        current.style.transition = "ease 0.5s";
+        next.style.transition = "ease 0.5s";
+
+        current.style.transform = `translateX(${-100 - 100 * position}%)`;
+        next.style.transform = `translateX(${-100 * nextPosition}%)`;
+
+        position = nextPosition;
+      }, 16);
+
+      timer = setTimeout(nextPic, 3000);
+    };
+
+    return root;
+  }
+
+  mountTo(parent) {
+    this.render().mountTo(parent);
+  }
 }
 
 // let component = (
@@ -101,9 +109,14 @@ class MyComponent {
 // );
 
 let component = (
-  <MyComponent>
-    <div>text</div>
-  </MyComponent>
+  <Carousel
+    data={[
+      "https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg",
+      "https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg",
+      "https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg",
+      "https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg",
+    ]}
+  ></Carousel>
 );
 
 component.mountTo(document.body);
