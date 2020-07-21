8336b209e51e66d7bdfeb76c19fbd03d2d9fe62f
v_fulinoliu <v_fulinoliu@tencent.com>
2020-07-21 22:15:38 +0800

    add animation

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import { create, Wrapper, Text } from "./create";
-import { Carousel } from "./carousel.view";
+import { Timeline, Animation, ColorAnimation } from "./animation.js";
+import { cubicBezier } from "./cubicBezier.js";
+let linear = t => t;
+let ease = cubicBezier(.25,.1,.25,1);
+// import { Carousel } from "./carousel.view";
 
 /*class MyComponent {
   constructor(config) {
@@ -34,7 +38,7 @@ import { Carousel } from "./carousel.view";
     this.render().mountTo(parent);
   }
 }*/
-/*
+
 class Carousel {
   constructor(config) {
     this.children = [];
@@ -67,33 +71,100 @@ class Carousel {
       let nextPosition = (position + 1) % this.data.length;
       let current = children[position];
       let next = children[nextPosition];
+      let tl = new Timeline;
+      // current.style.transition = "ease 0s";
+      // next.style.transition = "ease 0s";
 
-      current.style.transition = "ease 0s";
-      next.style.transition = "ease 0s";
+      // current.style.transform = `translateX(${-100 * position}%)`;
+      // next.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
 
-      current.style.transform = `translateX(${-100 * position}%)`;
-      next.style.transform = `translateX(${100 - 100 * nextPosition}%)`;
+      // setTimeout(function () {
+      //   current.style.transition = "ease 0.5s";
+      //   next.style.transition = "ease 0.5s";
 
-      setTimeout(function () {
-        current.style.transition = "ease 0.5s";
-        next.style.transition = "ease 0.5s";
+      //   current.style.transform = `translateX(${-100 - 100 * position}%)`;
+      //   next.style.transform = `translateX(${-100 * nextPosition}%)`;
 
-        current.style.transform = `translateX(${-100 - 100 * position}%)`;
-        next.style.transform = `translateX(${-100 * nextPosition}%)`;
+      //   position = nextPosition;
+      // }, 16);
 
-        position = nextPosition;
-      }, 16);
+      tl.add(new Animation(current.style, "transform", `${-500 * position}`, `${-500 -500 * position}`, 500, 0, linear, v => `translateX(${v}px)`))
+      tl.add(new Animation(next.style, "transform", `${500 - 500 * nextPosition}`, `${ -500 * nextPosition}`, 500, 0, linear, v => `translateX(${v}px)`))
+      position = nextPosition;
+      tl.start();
 
       setTimeout(nextPic, 3000);
     };
     setTimeout(nextPic, 3000);
+ 
+    /*root.addEventListener("mousedown", (event) => {
+      let startX = event.clientX,
+        startY = event.clientY;
+      let lastPosition = (position - 1 + this.data.length) % this.data.length;
+      let nextPosition = (position + 1) % this.data.length;
+
+      let current = children[position];
+      let last = children[lastPosition];
+      let next = children[nextPosition];
+
+      current.style.transition = "ease 0s";
+      last.style.transition = "ease 0s";
+      next.style.transition = "ease 0s";
+
+      current.style.transform = `translateX(${-500 * position}px)`;
+      last.style.transform = `translateX(${-500 - 500 * lastPosition}px)`;
+      next.style.transform = `translateX(${500 - 500 * nextPosition}px)`;
+
+      let move = (event) => {
+        current.style.transform = `translateX(${
+          event.clientX - startX - 500 * position
+        }px)`;
+        last.style.transform = `translateX(${
+          event.clientX - startX - 500 - 500 * lastPosition
+        }px)`;
+        next.style.transform = `translateX(${
+          event.clientX - startX + 500 - 500 * nextPosition
+        }px)`;
+      };
+      let up = (event) => {
+        let offset = 0;
+
+        if (event.clientX - startX > 250) {
+          offset = 1;
+        } else if (event.clientX - startX) {
+          offset = -1;
+        }
+
+        current.style.transition = "ease 0.5s";
+        last.style.transition = "ease 0.5s";
+        next.style.transition = "ease 0.5s";
+
+        current.style.transform = `translateX(${
+          offset * 500 - 500 * position
+        }px)`;
+        last.style.transform = `translateX(${
+          offset * 500 - 500 - 500 * lastPosition
+        }px)`;
+        next.style.transform = `translateX(${
+          offset * 500 + 500 - 500 * nextPosition
+        }px)`;
+
+        position = (position - offset + this.data.length) % this.data.length;
+
+        document.removeEventListener("mousemove", move);
+        document.removeEventListener("mouseup", up);
+      };
+      document.addEventListener("mousemove", move);
+      document.addEventListener("mouseup", up);
+    });*/
+
     return root;
   }
 
   mountTo(parent) {
     this.render().mountTo(parent);
   }
-}*/
+}
 
 // let component = (
 //   <Div id="a" class="b" style="width:100px;height:100px;background:red">
